3b6ec107450d7b9e1def9fbe910827eb1d1c3397
ketonBever0 <lacikurucz09@gmail.com>
2023-03-16 12:04:58 +0100

    frontend Vite React TS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container bg-black min-h-screen'>
+      <button className='btn btn-primary'>szia</button>
+
+    </div>
+  )
+}
+
+export default App
