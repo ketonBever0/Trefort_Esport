0268624c8e8baae0a8c73f8bd9c594eec4f81016
megnemveszithetek <richarszab56@gmail.com>
2023-04-17 11:53:56 +0200

    Db connect prisma
    Prisma adatbazis kapcsolat modulba szedese

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,15 +1,15 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
-
-    async enableShutdownHooks(app: INestApplication) {
-        this.$on('beforeExit', async () => {
-            await app.close();
+export class PrismaService extends PrismaClient {
+    constructor(){
+        super({
+            datasources: {
+                db: {
+                    url: "mysql://root:@localhost:3306/esport_portal"
+                },
+            },
         });
-    }   
+    }
 }
