d004abd1ca606b7f16e3269e8a9954db348d0a8a
Richard <richarszab56@gmail.com>
2023-04-19 22:08:25 +0200

    Http code status
    201-es kód cseréje bejelentkezésnél

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post, } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto, SignInDto } from "./dto";
 
@@ -10,7 +10,8 @@ export class AuthController {
     signup(@Body() dto: SignUpDto){
         return this.authService.signUp(dto);
     }
-
+    
+    @HttpCode(HttpStatus.OK) // 200
     @Post('signin')
     signin(@Body() dto: SignInDto){
         return this.authService.signIn(dto);
