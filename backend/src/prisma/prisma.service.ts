18c0edd94101f728b0162556898a3a87aa1f46d8
Richard <richarszab56@gmail.com>
2023-04-19 11:06:00 +0200

    config dto
    Biztonság miatt .env fájlból töltődik az adatbázias url

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
+    constructor(config: ConfigService){
         super({
             datasources: {
                 db: {
-                    url: "mysql://root:@localhost:3306/esport_portal"
+                    url: config.get('DATABASE_URL')
                 },
             },
         });
