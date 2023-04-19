f857bd28a626ddb2ffc7f361e2231a23367f6e8b
Richard <richarszab56@gmail.com>
2023-04-19 21:55:46 +0200

    Guard kivitele controller szintre
    Így minden végpont ami a users kontrollerben van jwt által lesz védve

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { Request } from 'express';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get('me')
     getMe(@GetUser() user: User) {
         return user;
