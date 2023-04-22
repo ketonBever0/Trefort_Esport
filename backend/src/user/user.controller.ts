bfab23a43e758c072f6079352ee1ff45215da3a3
Richard <richarszab56@gmail.com>
2023-04-22 17:20:08 +0200

    role based végpont védelem
    role alapú végpontok, mostmár jó

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator';
+import { GetUser, Roles } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 import { UserPatchDto } from './dto';
 import { UserService } from './user.service';
+import { RoleGuard } from 'src/auth/guard/role.guard';
 
-@UseGuards(JwtGuard)
+@UseGuards(JwtGuard, RoleGuard)
 @Controller('users')
 export class UserController {
     constructor(private userService: UserService){}
 
+    @Roles('admin')
     @Get('me')
     getMe(@GetUser() user: User) {
         return user;
