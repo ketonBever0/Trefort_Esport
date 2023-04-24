4bacb9682e56f06deb04341e7a62eb6cbbb123a3
megnemveszithetek <richarszab56@gmail.com>
2023-04-24 12:09:01 +0200

    event create
    Event létrehozása

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { EventService } from './event.service';
+import { JwtGuard } from 'src/auth/guard';
+import { EventDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('events')
 export class EventController {
     constructor (private eventService: EventService){}
-    @Get()
-    initEndpoint(){
-        return {
-            message: 'events'
-        }
+
+    @Post()
+    addNewEvent(@Body() dto: EventDto){
+        return this.eventService.addNewEvent(dto);
     }
 }
--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EventDto } from './dto';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+    constructor (private prismaService: PrismaService){}
+
+    async addNewEvent(dto: EventDto) {
+        try {
+            const event = await this.prismaService.event.create({
+                data: {
+                    name: dto.name,
+                    startDate: dto.startDate,
+                    endDate: dto.endDate,
+                    description: dto.description
+                }
+            });
+            return {
+                event
+            }   
+        } catch (error) {
+            if(error.meta.target.includes('key')) {
+                if(error.code === 'P2002') {
+                    throw new ForbiddenException('Ilyen néven már van esemény');
+                }
+            }
+        }
+    }
+    
+}
