1444492d9d92a79c7da658ca382e47d7da0f3b60
megnemveszithetek <richarszab56@gmail.com>
2023-04-25 10:38:27 +0200

    competition query
    Competition módosítások:
    getSingleCompetition
    getAllCompetition

--- backend/src/competition/competition.controller.ts
+++ backend/src/competition/competition.controller.ts
@@ -1,13 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { CompetitionService } from './competition.service';
 import { CompetitionDto } from './dto';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('competitions')
 export class CompetitionController {
     constructor(private competitionService: CompetitionService) {}
 
+    @Get('all')
+    getAllCompetition(){
+        return this.competitionService.getAllCompetition();
+    }
+
     @Post()
     createCompetition(@Body() dto: CompetitionDto){
-        
+        return this.competitionService.createCompetition(dto);
     }
+
+    @Get(':id')
+    getCompetition(
+        @Param('id', new ParseIntPipe())
+        id: number
+    ) {
+        return this.competitionService.getCompetition(id);
+    }
+
 }
