5fee81b82739c418a09ef7e9d44daf09e9213e86
its-ruben <szegediruben@gmail.com>
2023-04-26 00:41:40 +0200

    profil - komponensekkel kiegészítve, dolgozás a reszponzivitáson

--- frontend/src/pages/MyProfile.tsx
+++ frontend/src/pages/MyProfile.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
-import './input.css';
-import Button2 from '../ui/Button2';
+import './_css/input.css';
 import './_css/myProfile.css'
 
 function MyProfile() {
@@ -35,8 +34,19 @@ function MyProfile() {
                     className="rounded-circle img-fluid"
                     style={{ width: 150 }}
                   />
-                  <h5 className="my-3 h4">John Smith</h5>
-                  {
+                  
+                  <p className="my-3 lead text-white fw-bold text-center">john_smith83</p>
+                  <hr/>
+                  
+                  <div className='mt-20 d-flex justify-content-center gap-3'>
+                    <p className='lead fw-bold'>RANG: </p> <p className='d-flex align-items-center'>felhasználó</p>
+                  </div>
+                  <p className=''>ONLINE<span className='h6'>ㅤ</span><i className="mr-5 bi bi-circle-fill h6 position-absolute mt-4 m-auto text-success m-auto"></i></p>
+                  <p className=''>Legutóbb online: 26 perce<span className='h6'>ㅤ</span><i className="mr-5 bi bi-circle h6 position-absolute mt-4 m-auto text-success m-auto"></i></p>
+                  <p className=''>OFFLINE<span className='h6'>ㅤ</span><i className="mr-5 bi bi-circle-fill h6 position-absolute mt-4 m-auto text-secondary m-auto"></i></p>
+                  <p>BANNED<span className='h6'>ㅤ</span><i className="mr-5 bi bi-circle-fill h6 position-absolute mt-4 m-auto text-danger m-auto"></i></p>
+                  <hr/>
+             {
                     editDescr ?
                       <>
                         <div className="">
@@ -52,18 +62,37 @@ function MyProfile() {
                       </>
                       :
                       <>
-                        <p className='myProfileData p-15'>Just then her head struck against the roof of the hall: in fact she was now more than nine feet high, and she at once took up the little golden key and hurried off to the garden door...</p>
-                        <div className='row justify-content-center p-20'>
-                          <button onClick={() => { setEditDescr(true) }} className='col-sm-4 nk-btn nk-btn-rounded nk-btn-color-main-1'>Szerkesztés</button>
+                        <p className='myProfileData p-15 mb-5'>Just then her head struck against the roof of the hall: in fact she was now more than nine feet high, and she at once took up the little golden key and hurried off to the garden door...</p>
+                        <div className='row justify-content-center mb-20'>
+                          <button onClick={() => { setEditDescr(true) }} className='col-sm-4 col-md-3 col-lg-6 nk-btn nk-btn-rounded nk-btn-color-main-1'>Szerkesztés</button>
                         </div>
                         
                       </>
                   }
+                  <div className='bg-dark-4'>
+                    <div className='card-body d-flex justify-content-center gap-2'>
+                      <div className=' fw-bold'>
+                          Regisztráció dátuma:
+                      </div>
+                      <div className='col-lg-5 text-start'>
+                          2023. 02. 13.
+                      </div>
+                    </div>
+                    <hr/>
+                    <div className='card-body  d-flex justify-content-center gap-2 gap-lg-0'>
+                      <div className='col-lg-8 fw-bold'>
+                          Legutolsó bejelentkezés:
+                      </div>
+                      <div className='col-lg-5 text-start'>
+                          2023. 02. 13.
+                      </div>
+                    </div>
+                  </div>
 
 
 
                 </div>
-                <div className='p-30'>
+                <div className='p-30 bg-dark-2'>
                   <div className='h3'>#TAGEK</div>
                   <div className='badges'>
                     <span style={{ fontStyle: "italic" }} className='p-10 m-5 bg-main-1 lead text-light badge'>#hello</span>
@@ -79,40 +108,69 @@ function MyProfile() {
                 </div>
 
               </div>
-              <div className="card mb-4 mb-lg-0">
+              <div className="card mb-4 mb-lg-0 bg-transparent">
                 <div className="card-body p-0">
                   <ul className="list-group list-group-flush rounded-3 lead">
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-10">
-                      <i
-                        className="fab fa-github fa-2xl"
-                        style={{ color: "#333333" }}
-                      />
-                      <p className="mb-0">mdbootstrap</p>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-10">
+                    <li className="list-group-item bg-dark-4 d-flex justify-content-between align-items-center p-10 text-white">
                       <i
                         className="fab fa-discord fa-2xl"
                         style={{ color: "#5865F2" }}
                       />
-                      <p className="mb-0">mdbootstrap</p>
+                      <p className="mb-0">discorduser</p>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-10">
-                      <i
-                        className="fab fa-instagram fa-2xl"
-                        style={{ color: 'linear-gradient(to top right, #F58529,#FEDA77, #DD2A7B, #8134AF, #515BD4)' }}
-                      />
-                      <p className="mb-0">mdbootstrap</p>
+                    <li className="list-group-item bg-dark-2 d-flex justify-content-center gap-3 align-items-center p-10">
+                    <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                    alt="avatar"
+                    className="rounded-circle img-fluid"
+                    style={{ width: 50 }}
+                  />
+                      <p className='mb-0 lead fw-bold  text-main-1'>clan_name</p>
+                      <div className="mb-0 text-white">klán tagja</div>
                     </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center p-10">
-                      <i
-                        className="fab fa-facebook-f fa-2xl"
-                        style={{ color: "#3b5998" }}
-                      />
-                      <p className="mb-0">mdbootstrap</p>
-                    </li>
+                   
                   </ul>
                 </div>
               </div>
+              <table className="nk-table mt-30">
+                                <thead>
+                                    <tr className='text-center'>
+                                        <th colSpan={3}>Eredmények</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className='border-bottom'>
+                                        <th>Játék neve</th>
+                                        <th>Dátum</th>
+                                        <th className="text-center">Helyezés</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Valorant</td>
+                                        <td>2023.11.01.</td>
+                                        <td className="text-center"><strong>#3</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>LOL</td>
+                                        <td>2023.03.13.</td>
+                                        <td className="text-center"><strong>#7</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>LOL</td>
+                                        <td>2022.10.04.</td>
+                                        <td className="text-center"><strong>#5</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Rocket League</td>
+                                        <td>2022.08.19.</td>
+                                        <td className="text-center"><strong>#12</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Valorant</td>
+                                        <td>2022.03.14.</td>
+                                        <td className="text-center"><strong>#10</strong></td>
+                                    </tr>
+                                </tbody>
+                            </table>
             </div>
             <div className="col-lg-8">
               <div className='nk-decorated-h-2 h3 p-15 mt-40'><span> </span>Profil adataim<span> </span></div>
@@ -126,14 +184,14 @@ function MyProfile() {
                         {/* INPUTOK */}
                         <div className="row m-5">
                           <div className="col-sm-4 m-auto myProfileLabel">Név</div>
-                          <div className="col-sm-8 row gap-4 gap-md-0">
-                            <div className="col-md-4">
+                          <div className="col-sm-8 row gap-4">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                                 <label className='mb-5' htmlFor='vezeteknev'>Vezetéknév</label>
                                 <input type="text" id="vezeteknev" className="myform-control form-control required bg-dark p-10" required />
                               </div>
                             </div>
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                               <label className='mb-5' htmlFor='vezeteknev'>Keresztnév</label>
                                 <input type="text" id="keresztnev" className="myform-control form-control required bg-dark p-10" required />
@@ -143,27 +201,27 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Lakcím</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold fw-bold">Lakcím</div>
                           <div className="col-sm-8 row gap-4">
-                            <div className="col-md-3 col-7">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                               <label className='mb-10' htmlFor='iranyitoszam'>Irányítószám</label>
                                 <input type="text" id="iranyitoszam" className="myform-control form-control required bg-dark p-10" required />
                               </div>
                             </div>
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                               <label className='mb-10' htmlFor='telepules'>Település</label>
                                 <input type="text" id="telepules" className="myform-control form-control required bg-dark p-10" required />
                               </div>
                             </div>
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                               <label className='mb-10' htmlFor='kozterulet_nev'>Közterület neve</label>
                                 <input type="text" id="kozterulet_nev" className="myform-control form-control required bg-dark p-10" required />
                               </div>
                             </div>
-                            <div className="col-md-4 col-4">
+                            <div className="col-md-6 col-lg-5 col-9">
                               <div className="form-group myform-group">
                               <label className='mb-10' htmlFor='kozterulet_fajta'>Közterület jellege</label>
                                 <input type="text" id="kozterulet_fajta" className="myform-control form-control required bg-dark p-10" required />
@@ -179,22 +237,21 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Oktatási azonosító</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">Oktatási azonosító</div>
                           <div className="col-sm-8 row">
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-9 border">
                               <div className="form-group myform-group">
-                                <input type="text" id="omazonosito" className="myform-control form-control required bg-dark p-10" required />
+                                <input type="text" id="omazonosito" className="myform-control form-control required bg-dark p-10 d-flex align-items-center border" required />
                               </div>
-                              
                             </div>
-                             <i className="bi bi-info-circle-fill lead col-1"></i> <div className='col-sm-5'>A diákigazolvány hátulján található 11 jegyű számsor.</div>
+                             <i className="bi bi-info-circle-fill lead col-1"></i> <div className='col-md-6'>A diákigazolvány hátulján található 11 jegyű számsor.</div>
                           </div>
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">E-mail cím</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">E-mail cím</div>
                           <div className="col-sm-8 row">
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-9">
                               <div className="form-group myform-group">
                                 <input type="email" id="email" className="myform-control form-control required bg-dark p-10" required />
                               </div>
@@ -203,9 +260,9 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Felhasználónév</div>
+                          <div style={{hyphens:"auto",WebkitHyphens:'auto',msHyphens:'auto'}} className="col-sm-4 m-auto myProfileLabel fw-bold">Felhasználó­­név</div>
                           <div className="col-sm-8 row">
-                            <div className="col-md-4">
+                            <div className="col-md-6 col-9">
                               <div className="form-group myform-group">
                                 <input type="text" id="felhasznalonev" className="myform-control form-control required bg-dark p-10" required />
                               </div>
@@ -223,7 +280,7 @@ function MyProfile() {
                       <>
                         {/* DINAMIKUS ADATOK */}
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Név</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">Név</div>
                           <div className="col-sm-8 row">
                             <div className="sm-col-6 myProfileData">
                               Példa Név
@@ -232,7 +289,7 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Lakcím</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">Lakcím</div>
                           <div className="col-sm-8 row">
                             <div className="sm-sm-col-6 myProfileData">
                               Példa lakcím
@@ -241,7 +298,7 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Oktatási azonosító</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">Oktatási azonosító</div>
                           <div className="col-sm-8 row">
                             <div className="sm-col-6 myProfileData">
                               71608965663
@@ -250,7 +307,7 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">E-mail cím</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">E-mail cím</div>
                           <div className="col-sm-8 row">
                             <div className="sm-col-6 myProfileData">
                               email@gmail.com
@@ -259,7 +316,7 @@ function MyProfile() {
                         </div>
                         <hr />
                         <div className="row m-5">
-                          <div className="col-sm-4 m-auto myProfileLabel">Felhasználónév</div>
+                          <div className="col-sm-4 m-auto myProfileLabel fw-bold">Felhasználónév</div>
                           <div className="col-sm-8 row">
                             <div className="sm-col-6 myProfileData">
                               pelda123
@@ -283,7 +340,7 @@ function MyProfile() {
                             <>
                               <hr />
                               <div className="row m-5">
-                                <div className="col-sm-4 m-auto myProfileLabel">Régi jelszó</div>
+                                <div className="col-sm-4 m-auto myProfileLabel fw-bold">Régi jelszó</div>
                                 <div className="col-sm-8 row">
                                   <div className="col-md-6">
                                     <div className="form-group myform-group">
@@ -294,7 +351,7 @@ function MyProfile() {
                               </div>
                               <hr />
                               <div className="row m-5">
-                                <div className="col-sm-4 m-auto myProfileLabel">Új jelszó</div>
+                                <div className="col-sm-4 m-auto myProfileLabel fw-bold">Új jelszó</div>
                                 <div className="col-sm-8 row">
                                   <div className="col-md-6">
                                     <div className="form-group myform-group">
