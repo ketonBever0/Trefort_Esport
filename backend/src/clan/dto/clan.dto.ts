5f3f7298519de0d6b26012f3a85e5f258d62989a
Richard <richarszab56@gmail.com>
2023-05-02 10:06:07 +0200

    edit clan dto
    Propok megadása

--- backend/src/clan/dto/clan.dto.ts
+++ backend/src/clan/dto/clan.dto.ts
@@ -1,4 +1,22 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class ClanDto {
     
+    @IsNotEmpty()
+    @IsNumber()
+    leaderId: number;    
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    logo: string;
+
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    clanEmail: string;
 }
