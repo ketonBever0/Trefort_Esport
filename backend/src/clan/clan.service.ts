7a2901423f4b78d784f3bc18825d030ed106b25a
Richard <richarszab56@gmail.com>
2023-05-03 15:58:34 +0200

    clan crud
    Clan alapvető crud műveletei

--- backend/src/clan/clan.service.ts
+++ backend/src/clan/clan.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ClanDto } from './dto';
 
 @Injectable()
-export class ClanService {}
+export class ClanService {
+    constructor(private prismaService: PrismaService) {}
+
+    async createClan(dto: ClanDto) {
+        const clan = await this.prismaService.clan.create({
+            data: {
+                leaderId: dto.leaderId,
+                name: dto.name,
+                logo: dto.logo,
+                clanEmail: dto.clanEmail
+            }
+        });
+
+        return {
+            message: "Clan sikeresen létrehozva!",
+            clan
+        }
+    }
+
+    async getClans() {
+        const clans = await this.prismaService.clan.findMany({});
+
+        return {
+            clans
+        };
+    }
+
+    async getClanById(paramId: number) {
+        const clan = await this.prismaService.clan.findUnique({
+            where: {
+                id: paramId,
+            }
+        });
+
+        return {
+            clan
+        };
+    }
+
+    async updateClan(paramId: number, dto: ClanDto) {
+        const clan = await this.prismaService.clan.update({
+            where: {
+                id: paramId,
+            },
+            data: {
+                name: dto.name,
+                logo: dto.logo,
+                clanEmail: dto.clanEmail
+            }
+        });
+
+        return {
+            message: "Clan sikeresen frissítve!",
+            clan
+        };
+    }
+
+    async deleteClan(paramId: number) {
+        const clan = await this.prismaService.clan.update({
+            where: {
+                id: paramId,
+            },
+            data: {
+                status: "deleted"
+            }
+        });
+
+        return {
+            message: "Clan sikeresen törölve!",
+            clan
+        };
+    }
+
+}
